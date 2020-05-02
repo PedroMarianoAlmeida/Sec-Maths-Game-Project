40cb76e95d7034db753e7e6ccf9b3b76e4338bf0
PedroMarianoAlmeida <pedroalmeidaprogrammer@gmail.com>
2020-05-02 16:41:53 -0300

    Implemented

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 $('#counting-down').text(10);
+
+let createOperating = function() {
+    let number1 = Math.round( Math.random() * 10 );
+    let number2 = Math.round ( Math.random() * 10 );
+    let result = number1 + number2;
+    return( `${number1} + ${number2}`);
+}
+
+let setFirstRound = function(){
+    $('#operation').text(createOperating);
+}
+
+$(document).ready( function(){
+    setFirstRound();
+});
