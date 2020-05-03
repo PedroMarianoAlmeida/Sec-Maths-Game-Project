efa21e08f4c51ed4e90602ef88ba6e6a034144fd
PedroMarianoAlmeida <pedroalmeidaprogrammer@gmail.com>
2020-05-02 22:41:17 -0300

    implemented

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ let createOperating = function() {
     let number1 = Math.round( Math.random() * 10 );
     let number2 = Math.round ( Math.random() * 10 );
     let result = number1 + number2;
-    return( `${number1} + ${number2}`);
+    return( [`${number1} + ${number2}` , number1+number2]);
 }
 
+var currentOperation;
 let setFirstRound = function(){
-    $('#operation').text(createOperating);
+    currentOperation = createOperating();
+    $('#operation').text(currentOperation[0]);
 }
 
+let checkUserAnswer = function() {
+    let userAnswer = $('#user-answer').val();
+    //console.log(userAnswer);
+    if( Number(userAnswer) === Number(currentOperation[1]) ) {
+        //console.log('correct answer');
+        currentScore++;
+        $('#current-score').text(`Current score: ${currentScore}`);
+    }
+    else {
+        //console.log('Wrong answer');
+    }
+}
+
+var currentScore = 0;
 $(document).ready( function(){
     setFirstRound();
+    $(document).on('change' , '#user-answer', checkUserAnswer);
 });
